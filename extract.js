9ac8400a415013fed1554ae7ccab4fbe76ec775a
Ola Oredola <oredolaolamide@gmail.com>
2023-05-30 00:19:59 +0100

    changes to footer v1

---
+++ extract.js
@@ -0,0 +1,10 @@
+function extract() {
+// Extract job descriptions from upwork search page
+
+  let descriptions = document.querySelectorAll(
+    "span[data-test='job-description-text']"
+  );
+  let str = Array.from(descriptions).map((rslt) => rslt.textContent);
+
+  return str.join("; ");
+}
